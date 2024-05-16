a8c337d058a5d441fd551273d1408a19bdc76aae
Rickyvale <164905244+RiccardoValerio@users.noreply.github.com>
2024-05-16 20:05:44 +0200

    Aggiunta codici sorgenti numeri da 7 a 10 closes #9

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,16 +8,16 @@ package it.unipd.mtss;
 public class IntegerToRoman {
     
     public static String convert(int number){
-        String[] i = {"I", "II", "III", "IV", "V", "VI"};
-
-        if (number < 1 || number > 6) {
+        String[] i = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] x = {"", "X"};
+        if (number < 1 || number > 10) {
             throw new IllegalArgumentException(
-                "Input must be between 1 and 6"
+                "Input must be between 1 and 10"
             );
         }
 
-        int pos = number-1;
-        String res = i[pos];
-        return res;
+        String tens = x[(number % 100)/ 10];
+        String ones = i[number % 10];
+        return tens+ones;
     }    
 }
