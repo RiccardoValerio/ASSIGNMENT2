dcd0183595311751f9293490ce182edb24068879
milanRiccardoUnipd <riccardo.milan.4@studenti.unipd.it>
2024-05-17 17:27:14 +0200

    implementazione codice sorgente per i primi 100 numeri closes #12

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,19 +8,21 @@ package it.unipd.mtss;
 public class IntegerToRoman {
     
     private static String[] i = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    private static String[] x = {"", "X", "XX", "XXX", "XL", "L"};
+    private static String[] x = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static String[] c = {"", "C"};
     
     public static String convert(int number){
 
-        if (number < 1 || number > 50) {
+        if (number < 1 || number > 100) {
             throw new IllegalArgumentException(
-                "Input must be between 1 and 50"
+                "Input must be between 1 and 100"
             );
         }
 
+        String hundreds = c[(number % 1000) /100];
         String tens = x[(number % 100)/ 10];
         String ones = i[number % 10];
         
-        return tens+ones;
+        return hundreds+tens+ones;
     }    
 }
